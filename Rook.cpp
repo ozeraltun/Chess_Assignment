6b4892019992a63cb800c93acf13a5a4c774d728
ozer <ozer@ozer.altun>
2022-04-12 02:56:39 +0300

    rook bishop king are excluded

--- Rook.cpp
+++ Rook.cpp
@@ -6,10 +6,10 @@
 #include "ThreatCalculator.h"
 Rook::Rook(Color col, PieceType piece_type) : ChessPieceI(col, piece_type) {
   this->pieceValue = 5;
-  this->m_movementList.push_back(std::make_tuple(Direction::up, 8));
-  this->m_movementList.push_back(std::make_tuple(Direction::down, 8));
-  this->m_movementList.push_back(std::make_tuple(Direction::left, 8));
-  this->m_movementList.push_back(std::make_tuple(Direction::right, 8));
+  //this->m_movementList.push_back(std::make_tuple(Direction::up, 8));
+  //this->m_movementList.push_back(std::make_tuple(Direction::down, 8));
+  //this->m_movementList.push_back(std::make_tuple(Direction::left, 8));
+  //this->m_movementList.push_back(std::make_tuple(Direction::right, 8));
 }
 std::vector<position> Rook::getThreatList(std::array<std::array<ChessPieceI *, 8>, 8> objMap, const position objPosition) {
   Color col = this->m_Color;
